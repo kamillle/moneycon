c80714ea5321955ee31087fb22e27175a6f6890d
sainu <saino.katsutoshi@moneyforward.co.jp>
2020-05-30 13:38:02 +0900

    Update unicorn_config.rb and create unicorn_config.dev.rb

--- ruby/unicorn_config.dev.rb
+++ ruby/unicorn_config.dev.rb
@@ -1,6 +1,5 @@
 worker_processes 1
 preload_app true
-#listen "127.0.0.1:8080"
-listen "/home/isucon/private_isu/webapp/ruby/tmp/unicorn.sock"
+listen "127.0.0.1:8080"
 stdout_path "log/unicorn.log"
 stderr_path "log/unicorn.err.log"
